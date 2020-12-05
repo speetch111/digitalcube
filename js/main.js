fa59aadfd0ddce3815e0855d079cdab599870b69
speetch111 <58908023+speetch111@users.noreply.github.com>
2020-12-05 14:45:44 +0300

    apaptiv

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let header_height = $('.header__wrapper').innerHeight();
+
+function css_styles() {
+  if ($(window).width() > 750 && $(window).width() < 1280)
+    $('.header__nav').css({
+      'top': header_height,
+    });
+
+  $('.header').css({
+    'height': header_height,
+  });
+
+}
+
+function append_contacts() {
+  if ($(window).width() < 750) {
+    $('.mobile__menu-block').append($('.header__contacts'));
+
+    $('.mobile__menu-block').css({
+      'top': header_height,
+    });
+
+  }
+}
+
+$(window).resize(css_styles, append_contacts);
+
+$(function () {
+
+  css_styles();
+  append_contacts();
+
+  $('.mobile__hamburger').click(function () {
+    $('.mobile__menu-block').toggleClass('opened');
+    $('body').toggleClass('oh');
+    $(this).toggleClass('opened');
+  });
+
+  $('.header__nav-list li a').click(function () {
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).legth != 0) {
+      $('html, body').animate({
+        scrollTop: $(scroll_el).offset().top - 160
+      }, 500);
+
+      if ($(window).width() < 750) $('.mobile__hamburger').click();
+    }
+    return false;
+  });
+  
+});
